561a48f62c8d4065c3b388ff31bf99d6ef261b3e
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-11-23 08:06:51 +0000

    [Update]Users/index変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-
   def after_sign_in_path_for(resource)
     users_path(current_user.id)
   end
@@ -10,6 +8,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+  
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
