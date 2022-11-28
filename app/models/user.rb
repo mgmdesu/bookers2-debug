4efa146a9a12708fbb08c013665218f6359b2e45
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-11-28 06:51:30 +0000

    [add]relationshipsコントローラ作成/ビュー更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,18 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :relationships, source: :follower
   
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
+  
   has_one_attached :profile_image
   
   def get_profile_image
