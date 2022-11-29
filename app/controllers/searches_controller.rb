f417a5832cf705bb3368e469b69876fde7751cf1
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-11-29 07:21:50 +0000

    [update]コントローラ/モデル/ビュー追記

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,9 +4,9 @@ class SearchesController < ApplicationController
   def search
     @range = params[:range]
     if @range == "User"
-      @users == User.looks(params[:search], params[:word])
-    else
-      @books == Book.looks(params[:search], params[:word])
+      @users = User.looks(params[:search], params[:word])
+    else 
+      @books = Book.looks(params[:search], params[:word])
     end
   end
     
