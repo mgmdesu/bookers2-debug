25742e5e7aa4e6e8776f0224071cc07a89664fe3
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-11-29 02:13:34 +0000

    [finish]kadai4

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   
   has_many :followings, through: :relationships, source: :followed
-  has_many :followers, through: :relationships, source: :follower
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   
   def follow(user_id)
     relationships.create(followed_id: user_id)
