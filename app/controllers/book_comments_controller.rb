560e2da812f046e33c159648b6e2b70e4e855164
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-12-03 06:58:47 +0000

    [update]コメント機能の非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,13 +1,19 @@
 class BookCommentsController < ApplicationController
+  
   def create
-    @book = Book.find(params[:book_id])
+    @book_comment = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = @book.id
     comment.save
+    
+    @book_comment = BookComment.new
   end 
   
   def destroy
-    BookComment.find(params[:book_id]).destroy
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find_by(params[:id],params[:book_id])
+    @book_comment.destroy
+    
   end
   
   private
