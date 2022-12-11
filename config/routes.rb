6bd89cfe971fb7c46062438a17e46862972229a5
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-12-11 07:31:54 +0000

    [add]ouyoukadai9b search機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   resources :users, only: [:index,:show,:edit,:update] do
+    get "search", to: "users#search"
     resource :relationships, only: [:create, :destroy]
     get 'followings'=>'relationships#followings', as: 'followings'
     get 'followers'=>'relationships#followers', as: 'follower'
